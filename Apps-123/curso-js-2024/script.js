5a852ff958944b99756abcf123f8cef64cb20070
albecabrera <albe.cabrera@yahoo.de>
2024-02-10 14:15:28 +0100

    Readme actualizado2

--- Apps-123/curso-js-2024/script.js
+++ Apps-123/curso-js-2024/script.js
@@ -1,8 +1,13 @@
-// cortar un elemento de un array
+// Puede entrar esta persona al gimnasio deportivo?
 
-const array = ['Manzana', 'Naranja', 'Limón', 'Pera']
+let edadPersona = 25
 
-// índice, cuantos quiero cortar y que agrego
-array.splice(1,1, 'Cereza')
-
-console.log(array)
+if(edadPersona >= 18 && edadPersona <= 80){
+    console.log("Puede entrar al Gimnasio")
+}
+else if(edadPersona <=18){
+    console.log("No puede entrar al Gimnasio porque eres menor de edad")
+}
+else{
+    console.log("No puede entrar al Gimnasio porque eres mayor de 80 y no se recomienda")
+}
