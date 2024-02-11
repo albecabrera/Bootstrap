3f65eb9ebadb9c6df22ec0e806cd1fcaea34297b
albecabrera <albe.cabrera@yahoo.de>
2024-02-11 22:44:33 +0100

    Readme actualizado2

--- Apps-123/base-juego-adivinanza-js-master/script.js
+++ Apps-123/base-juego-adivinanza-js-master/script.js
@@ -1,29 +1,7 @@
-// Acá el juego selecciona un número al azar para que adivines
-let numeroAzar = Math.floor(Math.random() * 100) + 1
+// cuenta todos los números
+const numbers = [1, 2, 3]
+const numbers2 = [4, 5]
+               
+const allNumbers = [...numbers, ...numbers2]
 
-let numeroDeEntrada = document.getElementById("numeroDeEntrada")
-let mensaje = document.getElementById("mensaje")
-
-
-// Esta acción se va a ejecutar cuando se toque el botón chequear
-function chequearResultado() {
-    let numeroIngresado = parseInt(numeroDeEntrada.value);
-
-    if (numeroIngresado < 1 || numeroIngresado > 100 || isNaN(numeroIngresado)) {
-        mensaje.textContent = "Por favor, introduce un número válido entre 1 y 100";
-        mensaje.style.color = "red";
-        return
-    }
-
-    if (numeroIngresado === numeroAzar) {
-        mensaje.textContent = "¡Felicitaciones, has adivinado el número!";
-        mensaje.style.color = "green";
-        numeroDeEntrada.disabled = true;
-    } else if (numeroIngresado < numeroAzar) {
-        mensaje.textContent = "¡Más alto! El número es mayor al que dijiste";
-        mensaje.style.color = "red";
-    } else {
-        mensaje.textContent = "¡Más bajo! El número es menor al que dijiste";
-        mensaje.style.color = "red";
-    }
-}
+console.log(allNumbers) // [1, 2, 3, 4, 5]
