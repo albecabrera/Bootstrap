60138528096a631aa5ec9d18e318d3df2a44372e
albecabrera <albe.cabrera@yahoo.de>
2024-02-17 10:19:06 +0100

    Aprendiendo JS.

--- Desktop/AprendeJS/main.js
+++
@@ -1,7 +0,0 @@
-let numero1 = 23;
-let numero2 = 13;
-
-let resultado = numero1 > numero2;
-
-document.write(numero1 != numero2)
-
---
+++ Desktop/AprendeJS/script.js
@@ -0,0 +1,12 @@
+
+let i = 6;
+
+for (i;i >= 0; i--) {
+    document.write(i + "<br>")
+    if (i == 3) {
+        continue;
+        
+    }
+}
+
+document.write(i)
