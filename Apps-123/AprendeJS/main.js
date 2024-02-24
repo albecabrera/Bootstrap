13155212b8b2730dc8fdfc8c7d792daf807ff268
albecabrera <albe.cabrera@yahoo.de>
2024-02-24 22:55:59 +0100

    actualizando mensaje 24 de febrero.

--- Apps-123/AprendeJS/main.js
+++ Apps-123/AprendeJS/main.js
@@ -1,1 +1,35 @@
-console.log('Operadores');
+let numeroDia = prompt ('Ingresa el día de la semana:');
+
+switch (numeroDia){
+    case '1':
+        document.write('<h1>El día de hoy es<br>LUNES</h1>');
+        break;
+
+    case '2':
+        document.write('<h1>El día de hoy es<br>MARTES</h1>');
+        break;
+
+    case '3':
+        document.write('<h1>El día de hoy es<br>MIERCOLES</h1>');
+        break;
+    
+    case '4':
+        document.write('<h1>El día de hoy es<br>JUEVES</h1>');
+        break;
+    
+    case '5':
+        document.write('<h1>El día de hoy es<br>VIERNES</h1>');
+        break;
+   
+    case '6':
+        document.write('<h1>El día de hoy es<br>SABADO</h1>');
+        break;
+
+   case '7':
+        document.write('<h1>El día de hoy es<br>DOMINGO</h1>');
+        break;
+    
+    default:
+        document.write('<h1>Día desconocido</h1>');
+}
+
