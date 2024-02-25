cec0972033019c29b0238f3dd4d6b6a57465c44b
albecabrera <albe.cabrera@yahoo.de>
2024-02-25 01:02:25 +0100

    actualizando mensaje 24 de febrero.

---
+++ Apps-123/AprendeJS/main2.js
@@ -0,0 +1,25 @@
+let numeros = [
+    'uno',
+    'dos',
+    'tres'
+];
+
+console.log(numeros); 
+numeros.push('cuatro');
+console.log(numeros);
+
+numeros.pop();
+console.log(numeros);
+
+numeros.unshift('cero');
+console.log(numeros);
+
+numeros.shift();
+console.log(numeros);
+
+let matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+console.log(matriz[1][2]);
