0426565ae61abc3d9da8888083367ce8878bb614
albecabrera <albe.cabrera@yahoo.de>
2024-02-25 01:36:49 +0100

    actualizando mensaje 24 de febrero.

---
+++ Apps-123/AprendeJS/main3.js
@@ -0,0 +1,30 @@
+// // function saludoInicial(name="David"){
+// //     document.write(`Hola, mi nombre es §{name}<br>`);
+// // }
+
+// // saludoInicial("Steve");
+// // saludoInicial();
+
+
+// function sumarNumeros(a, b){
+//     let c = 1;
+//     let suma = a + b + c;
+//     c = 5;
+//     document.write(suma + "<br>");
+// }
+
+// sumarNumeros(8, 30);
+// document.write(c);
+
+
+
+let usuario = {
+ nombre: "David",
+ edad: 28,
+ estudiante: true
+}
+
+console.log(usuario);
+console.log(usuario.nombre);
+console.log(usuario.edad);
+console.log(usuario.estudiante);
