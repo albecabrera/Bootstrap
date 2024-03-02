46bd40a4e6ec273873cb86bb56c356dd0f8088cf
albecabrera <albe.cabrera@yahoo.de>
2024-03-02 20:11:00 +0100

    actualizado 2 de marzo

--- Desktop/js-for-react/index.js
+++ Desktop/js-for-react/index.js
@@ -1,7 +1,50 @@
-function hello(){
-   return function() {
-    return 'hola mundo' 
-}
+// function add(x, y) {
+//     return x + y
+// }
+// console.log(add(10, 20))
+
+
+// ---------defalut visitParameters----------
+
+// function add(x, y) {
+//     return x + y
+// }
+
+// console.log(add(10, 50))
+
+// function add(x=23.3, y=0) {
+//     return x + y
+// }
+
+// console.log(add())
+
+// const user = {
+//     name: 'sajal',
+//     lastname: 'singh',
+//     age: 30, 
+//     address: {
+//         contry: 'colombia',
+//         city: 'bogota',
+//         street: 'calle 1'
+//     }, 
+//    friends: ['brandon', 'elena'],
+//    active: true,
+//    sendMail: function () {
+//     return 'sending email'  
+//    }
+// }
+
+// console.log(user.address.city)
+// console.log(user.friends)
+
+const name = 'laptop'
+const price = 3000
+
+const newProduct = {
+    name, 
+    price
+
 }
 
-console.log(hello()())
+console.log(newProduct)
+
