72406a116838b9e480007611b94026e3f2a66b87
albecabrera <albe.cabrera@yahoo.de>
2024-03-09 23:21:46 +0100

    actualizado el 9 de marzo de 2024

---
+++ Apps-123/miPrimerProyectoJs/miPrimerProyectoDeNodeJs/if-else-2.js
@@ -0,0 +1,17 @@
+// If else + if else
+
+let nota = 5;
+
+if (nota === 5) {
+  console.log("Excelente");
+} else if (nota === 4) {
+  console.log("Muy bueno");
+} else if (nota === 3) {
+  console.log("Bueno");
+} else if (nota === 2) {
+  console.log("Regular");
+} else if (nota === 1) {
+  console.log("Malo");
+} else {
+  console.log("Error");
+}
---
+++ Apps-123/miPrimerProyectoJs/miPrimerProyectoDeNodeJs/if-else.js
@@ -0,0 +1,11 @@
+// Bifurcaciones if...else
+
+if (true) {
+  console.log("Es verdadero");
+}
+
+if (false) {
+  console.log("Es verdadero");
+} else {
+  console.log("Es falso");
+}
